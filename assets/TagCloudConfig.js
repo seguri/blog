5d4895da4675036ca3c1c20f60a1abdf4ae52677
Marco Seguri <seguri@users.noreply.github.com>
2024-12-14 01:17:14 +0100

    Add tagcloud

---
+++ assets/TagCloudConfig.js
@@ -0,0 +1,22 @@
+TagCloud(
+  ".tagcloud-container",
+  [
+    "Java",
+    "Spring Boot",
+    "Hibernate",
+    "Kafka",
+    "Docker",
+    "Kubernetes",
+    "JavaScript",
+    "React",
+    "Cloud",
+    "Microservices",
+    "Python",
+    "Django",
+  ],
+  {
+    useContainerInlineStyles: false,
+    useItemInlineStyles: false,
+    radius: 100,
+  },
+);
